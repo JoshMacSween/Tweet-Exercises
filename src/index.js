4680d25700c0f0dd4725441c456848f4d1bab3e0
Joshua MacSween <joshmacsween@gmail.com>
2020-08-03 18:29:50 -0600

    working tweet component

--- src/index.js
+++ src/index.js
@@ -6,11 +6,18 @@ function Tweet() {
   return (
     <div className='tweet'>
       <Avatar />
-      <div className="content">
-        <Author />
-        <Message />
+        <div className='content'>
+          <Author /><Time />
+          <Message />
+        <div className="buttons">
+          <ReplyButton />
+          <RetweetButton />
+          <LikeButton />
+          <MoreOptionsButton />
+        </div>
       </div>
-    </div>)
+    </div>
+  )
 }
 
 function Avatar() {
@@ -24,20 +31,30 @@ function Avatar() {
 }
 
 function Message() {
-  return(
-    <div className="message">
-      This message is less than 140 characters.
-    </div>
+  return (
+    <div className='message'>This message is less than 140 characters.</div>
   )
 }
 
 function Author() {
-  return(
-    <span className="author">
-      <span className="name">Your Name</span>
-      <span className="handle">@yourhandle</span>
+  return (
+    <span className='author'>
+      <span className='name'>Your Name</span>
+      <span className='handle'>@yourhandle</span>
     </span>
   )
 }
+
+const Time = () => <span className='time'>3 hours ago</span>
+
+const ReplyButton = () => <i className='fa fa-reply reply-button' />
+
+const RetweetButton = () => <i className='fa fa-retweet retweet-button' />
+
+const LikeButton = () => <i className='fa fa-heart like-button' />
+
+const MoreOptionsButton = () => (
+  <i className='fa fa-ellipsis-h more-options-button' />
+)
 
 ReactDOM.render(<Tweet />, document.querySelector('#root'))
