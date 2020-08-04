788a9aab01e84188dd5b6d6527d00279e5e95089
Joshua MacSween <joshmacsween@gmail.com>
2020-08-03 18:07:13 -0600

    init

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+function Tweet() {
+  return (
+    <div className='tweet'>
+      <Avatar />
+      <div className="content">
+        <Author />
+        <Message />
+      </div>
+    </div>)
+}
+
+function Avatar() {
+  return (
+    <img
+      src='https://www.gravatar.com/avatar/nothing'
+      className='avatar'
+      alt='avatar'
+    />
+  )
+}
+
+function Message() {
+  return(
+    <div className="message">
+      This message is less than 140 characters.
+    </div>
+  )
+}
+
+function Author() {
+  return(
+    <span className="author">
+      <span className="name">Your Name</span>
+      <span className="handle">@yourhandle</span>
+    </span>
+  )
+}
+
+ReactDOM.render(<Tweet />, document.querySelector('#root'))
